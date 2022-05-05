38c351dd3438f1457bbc370cf78ee78dfc866820
Ville Tirkkonen <Ville.Tirkkonen3@edu.savonia.fi>
2022-05-05 20:20:43 +0300

    Vaiheessa

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,50 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+
+
+
+//Haetaan data local storagesta
+
+const getDatafromLS=()=>{
+  const data = localStorage.getItem('tuotteet');
+  if(data){
+    return JSON.parse(data);
+  }
+  else{
+    return []
+  }
+}
+
 
 export const App = () => {
+
+  const [tuotteet, setTuote]=useState(getDatafromLS());
+
+  const [tnimi, setTuotenimi]=useState('');
+  const [thylly, setHylly]=useState('');
+  const [lkm, setLukumäärä]=useState('');
+
+const handleLisaaTuoteSubmit=(e)=>{
+  e.preventDefault();
+
+  let tuote={
+    tnimi,
+    thylly,
+    lkm
+  }
+
+  setTuote([...tuotteet,tuote]);
+  setTuotenimi('');
+  setHylly('');
+  setLukumäärä('');
+
+}
+
+//Tallennettaan local storageen
+
+useEffect(()=>{
+  localStorage.setItem('tuotteet',JSON.stringify(tuotteet));
+},[tuotteet])
+
   return (
     <div className='wrapper'>
       <h1>Tuotevarasto</h1>
@@ -8,16 +52,23 @@ export const App = () => {
       <div className='main'>
 
         <div className='form-container'>
-          <form autoComplete="off" className='form-group'>
+          <form autoComplete="off" className='form-group'
+          onSubmit={handleLisaaTuoteSubmit}>
 
             <label>Tuotenimi</label>
-            <input type="text" className='form-control' required></input>
+            <input type="text" className='form-control' required
+            onChange={(e)=>setTuotenimi(e.target.value)} value={tnimi}></input>
+
             <br></br>
-            <label>Tuotekuvaus</label>
-            <input type="text" className='form-control' required></input>
+            <label>Hylly</label>
+            <input type="text" className='form-control' required
+            onChange={(e)=>setHylly(e.target.value)} value={thylly}></input>
+
             <br></br>
-            <label>Tuote-ID</label>
-            <input type="text" className='form-control' required></input>
+            <label>Lukumäärä</label>
+            <input type="text" className='form-control' required
+            onChange={(e)=>setLukumäärä(e.target.value)} value={lkm}></input>
+
             <br></br>
             <button type="submit" className='btn btn-success btn-md'> Lisää</button>
           </form>
@@ -25,7 +76,7 @@ export const App = () => {
         </div>
 
         <div className='view-container'>
-
+        {tuotteet.length < 1 && <div> Ei tuoteitta</div>}
 
         </div>
       </div>
